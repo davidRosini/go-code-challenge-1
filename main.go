4f05ddb3385a8542f4b694274d8c4073bc6580a7
David Aaron Rosini <waronr@gmail.com>
2026-04-20 15:47:50 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import readerAdapter "codechallenge.test/adapter"
+import (
+	"os"
+
+	"codechallenge.test/adapter"
+	"codechallenge.test/service"
+	"codechallenge.test/usecase"
+)
 
 func main() {
-	readerAdapter.InitReader()
+	buyOperationUsecase := usecase.NewBuyOperationUsecase()
+	sellOperationUsecase := usecase.NewSellOperationUsecase(0.2)
+	calculateTaxService := service.NewCalculateTaxService(buyOperationUsecase, sellOperationUsecase)
+	calculateTaxHandler := adapter.NewCalculateTaxHandler(os.Stdin, os.Stdout, calculateTaxService)
+	calculateTaxHandler.Execute()
 }
